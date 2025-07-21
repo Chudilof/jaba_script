1e5ee1644ca353676f57ff742b8ae5856350b5c6
Igor1 <chudilof001@gmail.com>
2025-07-21 23:36:23 +0300

    Виправлення відповідно до зауважень

--- script/Homework1.js
+++ script/Homework1.js
@@ -7,9 +7,9 @@ if (age != +age){
     console.log("ІДИ ЗВІДСИ, БРЕХЛО!");
 }else if(+age >= 100){
     console.log("Вітаю тебе, довгожитель!")
-}else if(+age == 1 || +age == 21 || +age == 31 || +age == 41 || +age == 51 || +age == 61 || +age == 71 || +age == 81 || +age == 91){
+}else if(+age%10 == 1 && +age!=11){
     console.log("Тобі "+age+" рік!");
-}else if(+age>=2 && +age<=4 || +age>=22 && +age<=24 || +age>=32 && +age<=34 || +age>=42 && +age<=44 || +age>=52 && +age<=54 || +age>=62 && +age<=64 || +age>=72 && +age<=74 || +age>=82 && +age<=84 || +age>=92 && +age<=94){
+}else if(+age%10>=2 && +age%10<=4 && !([12, 13, 14].includes(+age % 100))){
     console.log("Тобі "+age+" роки!")
 }else{
     console.log("Тобі "+age+" років!")
